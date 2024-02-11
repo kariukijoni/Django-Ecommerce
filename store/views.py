45dd6319083c914eace4f602b282869642bc623d
kariukijoni <kariukijoni.com>
2024-02-11 15:46:16 +0300

    Added cart view listing

--- store/views.py
+++ store/views.py
@@ -8,7 +8,16 @@ def store(request):
      return render(request, 'store/store.html', context)
 
 def cart(request):
-     context = {}
+     if request.user.is_authenticated:
+          customer=request.user.customer
+          order,created=Order.objects.get_or_create(customer=customer,complete=False)
+          items=order.orderitem_set.all()
+     else:
+          items=[]
+          # load data for un_authenticated users
+          order={'get_cart_total':0,'get_cart_items':0}
+     context = {'items':items,'order':order}
+     
      return render(request, 'store/cart.html', context)
 
 def checkout(request):
